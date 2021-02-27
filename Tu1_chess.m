9b1c5c9a558f47d3f7e71bd1c89f5601505fa989
githubfsfs <52351247+githubfsfs@users.noreply.github.com>
2021-02-27 10:32:52 +0800

    Add files via upload

---
+++ Tu1_chess.m
@@ -0,0 +1,12 @@
+%% Activity 1: Basic Image Operations
+close all;clear all;clc;
+% imshow('chess.png')
+% xlabel('column')
+% ylabel('row')
+% impixelinfo;
+% 
+% row =input('Enter the row:');
+% column =input('Enter the column:');
+row = 5;
+column = 6;
+im_board = board_position(row,column);
